5879f507d639e2350ad4da6a5bf97703b08f99c7
Christian Kuehn <dev@cyanide.de>
2016-10-27 09:26:55 +0200

    follow setup-guide

--- src/main/java/de/cyanide/SbaApplication.java
+++ src/main/java/de/cyanide/SbaApplication.java
@@ -1,8 +1,14 @@
 package de.cyanide;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 @SpringBootApplication
 public class SbaApplication {
 
