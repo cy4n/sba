6a0ddd3790551b49c486b0b7e7ac3d4b2de22752
Christian Kuehn <kuehn@synyx.de>
2016-10-27 09:02:55 +0200

    Initial commit: Add SpringbootStarter project

---
+++ src/main/java/de/cyanide/SbaApplication.java
@@ -0,0 +1,12 @@
+package de.cyanide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaApplication.class, args);
+	}
+}
